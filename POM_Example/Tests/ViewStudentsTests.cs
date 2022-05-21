7b46adcc3e3b4877ad94c96b0b339ba6d2dc248a
Mihail Mitev <95048979+mihailmitev@users.noreply.github.com>
2022-05-21 16:19:05 +0300

    Add files via upload

---
+++ POM_Example/Tests/ViewStudentsTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using POM_Example.PAges;
+
+namespace POM_Example.Tests
+{
+    public class ViewStudents
+    {
+        private IWebDriver driver;
+        [SetUp]
+        public void Setup()
+        {
+            this.driver = new ChromeDriver();
+        }
+        [TearDown]
+        public void ShutDownBrowser()
+        {
+
+            driver.Quit();
+        }
+        [Test]
+        public void TitlePresenceTest()
+        {
+            driver.Navigate().GoToUrl("https://mvc-app-node-express.nakov.repl.co/");
+            driver.Manage().Window.Size = new System.Drawing.Size(1552, 832);
+            Assert.That(driver.FindElement(By.CssSelector("h1")).Text, Is.EqualTo("Students Registry"));
+        }
+        [Test]
+        public void SecondTestStudents()
+        {
+            driver.Navigate().GoToUrl("https://mvc-app-node-express.nakov.repl.co/");
+            driver.Manage().Window.Size = new System.Drawing.Size(1552, 832);
+            driver.FindElement(By.LinkText("View Students")).Click();
+            driver.FindElement(By.CssSelector("ul")).Click();
+            var elements1 = driver.FindElements(By.CssSelector("li:nth-child(2)"));
+            var elements2 = driver.FindElements(By.CssSelector("li:nth-child(3)"));
+            var elements3 = driver.FindElements(By.CssSelector("li:nth-child(1)"));
+            int sumOfAllElements = elements1.Count + elements2.Count + elements3.Count;
+
+            Assert.True(sumOfAllElements >= 3);
+        }
+
+    }
+}
